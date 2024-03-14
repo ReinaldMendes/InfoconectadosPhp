5251fd292c47878a1c9ba5d91cce0945056f45ff
Reinald Mendes <reinald.2967@aluno.pr.senac.br>
2024-03-14 20:34:32 -0300

    3 commit

--- adm/inc/footer.inc.php
+++ adm/inc/footer.inc.php
@@ -1,5 +1,8 @@
 <footer>
-    <h1>Aqui será meu rodapé</h1>
+    <h1>Aqui será meu rodapé...</h1>
  </foter>
+ <script type ="text/javascript" src="js/jquery-3.7.1.min.js"> </script>
+ <script type ="text/javascript" src="js/bootstrap.min.js"> </script>
+ </div>
 </body>
 </html>
--- adm/inc/header.inc.php
+++ adm/inc/header.inc.php
@@ -3,9 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <title>Infoconectados Php</title>
+    <style>
+    .small-image {
+      max-width: 10%; /* Defina o tamanho máximo desejado */
+      height: auto; /* Mantenha a proporção da imagem */
+    }
+  </style>
 </head>
 <body>
+<div class="container">
      <header>
-        <h1> Aqui será meu cabeçalho </h1>
+        
+        <div class="navbar navbar-dark bg-dark">
+        <div class="container d-flex justify-content-between">   
+        <a href="index.php" class="navbar-brand">
+        <img src="img/logo.png" class="img-fluid small-image" alt="Logo Infoconectados">
+      
+        <strong> Infoconectados</strong>
+        </a>
+        <nav>
+            <a href="#"class="text-white navbar-brand">login</a>
+        </nav>
+        </div>
+        </div>
     </header>
