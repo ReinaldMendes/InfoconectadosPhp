1482c3d1e9b563c0d41eeaa6577ac5a9cdf10720
Reinald Mendes <reinald.2967@aluno.pr.senac.br>
2024-04-04 20:34:57 -0300

    Novo

--- adm/inc/footer.inc.php
+++ adm/inc/footer.inc.php
@@ -13,6 +13,7 @@
     float:right;
     list-style-type: none;
 }
+
 </style>
 <div class="container">
   <footer id="footer" class="py-3 mt-5 bg-dark text-white">
@@ -25,12 +26,12 @@
         <li><a href="http://www.instagram.com/reinaldmendes" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/instagram.svg" width="32" height="32" /></a></li>
         <li><a href="https://www.linkedin.com/in/reinald-mendes-b712b9182/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/linkedin.svg" width="32" height="32" /></a></li>
         <li><a href="https://www.twitter.com/reinaldmendes" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/twitter.svg" width="32" height="32" /></a></li>
-        <li><a href="http://lattes.cnpq.br/4852567676685113" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/twitter.svg" width="32" height="32" /></a></li>
+        <li><a href="http://lattes.cnpq.br/4852567676685113" target="_blank" rel="noreferrer"><img src="https://camo.githubusercontent.com/e2c4d3ba2454bde6607313169394244a3e3d8e82dfcb4d47fd1dda41b3e8150a/68747470733a2f2f6172717569766f2e756e69666573702e62722f696d616765732f69636f6e2f69636f6e5f6c61747465732e737667" width="32" height="32" /></a></li>
           
           
         </ul>
       </div>
-      <div class="col-12 col-md-7 text-center text-md-right">
+       <div class="col-12 col-md-7 text-left text-md-left">
         <p class="mb-0">&copy; Design: <a href="index.php" class="text-white">Infoconectados</a></p>
       </div>
     </div>
--- adm/index.php
+++ adm/index.php
@@ -1,7 +1,23 @@
 <?php
 include 'inc/header.inc.php';
 ?>
-     <h1> Aqui será o corpo do site </h1>
+
+<style>
+  h1 {
+    text-align: center; /* Centraliza o conteúdo na horizontal */
+  }
+  .img {
+    display: block; /* Garante que a imagem será tratada como um bloco */
+    margin: 0 auto; /* Centraliza a imagem na horizontal */
+  }
+  body{
+    background-color: black;
+  }
+
+</style>
+
+<h1>Infoconectados Admin</h1>
+<img src="img/logo.png" alt="Logo Infoconectados"> </a>
 
 <?php
 include 'inc/footer.inc.php';
