17fb8f5729362a85bd79a49259b173f2b1886126
Reinald Mendes <reinald.2967@aluno.pr.senac.br>
2024-04-03 21:30:48 -0300

    Commit novo

--- adm/inc/footer.inc.php
+++ adm/inc/footer.inc.php
@@ -1,8 +1,42 @@
-<footer>
-    <h1>Aqui será meu rodapé...</h1>
- </foter>
- <script type ="text/javascript" src="js/jquery-3.7.1.min.js"> </script>
- <script type ="text/javascript" src="js/bootstrap.min.js"> </script>
- </div>
+<style>
+  .small-image {
+    max-width: 50px; /* Alterado para um tamanho mais adequado */
+    height: auto; /* Mantenha a proporção da imagem */
+    margin-right: 10px; /* Adicionado um espaçamento entre a imagem e as redes sociais */
+  }
+  .social li {
+    display: inline;
+    margin-right: 10px; /* Espaçamento entre os ícones das redes sociais */
+  }
+</style>
+<div class="container">
+  <footer id="footer" class="py-3 mt-5 bg-dark text-white">
+    <div class="row">
+      <div class="col-12 col-md-6">
+        <img src="img/logo.png" class="img-fluid small-image" alt="Logo Infoconectados">
+        <ul class="social">
+          <li><a href="https://www.twitter.com/reinaldmendes"><img src="img/twitter.png" alt="Twitter" class="small-image"></a></li>
+          <li><a href="https://www.facebook.com/ReinaldMendes18"><img src="img/facebook.png" alt="Facebook" class="small-image"></a></li>
+          <li><a href="http://www.instagram.com/reinaldmendes"><img src="img/instagram.png" alt="Instagram" class="small-image"></a></li>
+          <li><a href="https://www.github.com/reinaldmendes"><img src="img/github.png" alt="GitHub" class="small-image"></a></li>
+          <li><a href="http://lattes.cnpq.br/4852567676685113"><img src="img/lattes.png" alt="Lattes" class="small-image"></a></li>
+          <li><a href="https://www.linkedin.com/in/reinald-mendes-b712b9182/"><img src="img/linkedin.png" alt="Linkedin" class="small-image"></a></li>
+        </ul>
+      </div>
+      <div class="col-12 col-md-7 text-center text-md-right">
+        <p class="mb-0">&copy; Design: <a href="index.php" class="text-white">Infoconectados</a></p>
+      </div>
+    </div>
+  </footer>
+
+<script type ="text/javascript" src="js/jquery-3.7.1.min.js"> </script>
+<script type ="text/javascript" src="js/bootstrap.min.js"> </script>
+</div>
 </body>
 </html>
+
+
+
+
+
+
