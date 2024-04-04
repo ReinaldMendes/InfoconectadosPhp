c2b55dded00e6326d072fbbc0c51e99bf5962593
Reinald Mendes <reinald.2967@aluno.pr.senac.br>
2024-04-04 19:54:07 -0300

    commit NOVO

--- adm/inc/footer.inc.php
+++ adm/inc/footer.inc.php
@@ -8,6 +8,11 @@
     display: inline;
     margin-right: 10px; /* Espaçamento entre os ícones das redes sociais */
   }
+  .social{
+    position: relative;
+    float:right;
+    list-style-type: none;
+}
 </style>
 <div class="container">
   <footer id="footer" class="py-3 mt-5 bg-dark text-white">
@@ -15,12 +20,14 @@
       <div class="col-12 col-md-6">
         <img src="img/logo.png" class="img-fluid small-image" alt="Logo Infoconectados">
         <ul class="social">
-          <li><a href="https://www.twitter.com/reinaldmendes"><img src="img/twitter.png" alt="Twitter" class="small-image"></a></li>
-          <li><a href="https://www.facebook.com/ReinaldMendes18"><img src="img/facebook.png" alt="Facebook" class="small-image"></a></li>
-          <li><a href="http://www.instagram.com/reinaldmendes"><img src="img/instagram.png" alt="Instagram" class="small-image"></a></li>
-          <li><a href="https://www.github.com/reinaldmendes"><img src="img/github.png" alt="GitHub" class="small-image"></a></li>
-          <li><a href="http://lattes.cnpq.br/4852567676685113"><img src="img/lattes.png" alt="Lattes" class="small-image"></a></li>
-          <li><a href="https://www.linkedin.com/in/reinald-mendes-b712b9182/"><img src="img/linkedin.png" alt="Linkedin" class="small-image"></a></li>
+        <a href="https://www.facebook.com/ReinaldMendes18" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/facebook.svg" width="32" height="32" /></a></li>
+        <li><a href="https://www.github.com/reinaldmendes" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/github.svg" width="32" height="32" /></a></li>
+        <li><a href="http://www.instagram.com/reinaldmendes" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/instagram.svg" width="32" height="32" /></a></li>
+        <li><a href="https://www.linkedin.com/in/reinald-mendes-b712b9182/" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/linkedin.svg" width="32" height="32" /></a></li>
+        <li><a href="https://www.twitter.com/reinaldmendes" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/twitter.svg" width="32" height="32" /></a></li>
+        <li><a href="http://lattes.cnpq.br/4852567676685113" target="_blank" rel="noreferrer"><img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/socials/twitter.svg" width="32" height="32" /></a></li>
+          
+          
         </ul>
       </div>
       <div class="col-12 col-md-7 text-center text-md-right">
