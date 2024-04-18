ee39573f811a4ed2be8890d9bca2fe4b9da9f3ac
Reinald Mendes <reinald.2967@aluno.pr.senac.br>
2024-04-17 22:03:00 -0300

    Commit apresentação

--- adm/index.php
+++ adm/index.php
@@ -5,10 +5,11 @@ include 'inc/header.inc.php';
   body {
     background-color: #ccc;
     background-image: url('img/logo.png'); /* substitua 'caminho/para/sua/imagem.jpg' pelo caminho correto para sua imagem */
-    background-size: small; /* garante que a imagem cubra todo o fundo */
+    background-size: 500px 300px;; /* garante que a imagem cubra todo o fundo */
     background-position: center; /* centraliza a imagem no fundo */
     background-repeat: no-repeat; /* evita repetição da imagem */
     color: white; /* define a cor do texto como branca para melhor legibilidade */
+   
   }
 
   h1 {
@@ -21,17 +22,53 @@ include 'inc/header.inc.php';
     margin: 0 auto; /* Centraliza a imagem na horizontal */
   }
 
-  .container-fluid {
-    padding-top: 50px; /* Ajuste para afastar o conteúdo do topo, para que não fique escondido pela imagem de fundo */
-  }
+ 
 </style>
 
 <style>
-    .login-container {
+    
+.login-container {
+        max-width: 400px;
+        margin:  0 auto;
+        padding: 50px;
+        background-color: #3a6310;
+        border-radius: 8px;
+        box-shadow: 0 12px 50px rgba(0, 0, 0, 0.1);
+        position: center;
+    }
+    .login-container h1 {
         text-align: center;
+    }
+    .login-container form {
+        margin-top: 20px;
+    }
+    .login-container label {
+        display: block;
+        margin-bottom: 5px;
+    }
+    .login-container input[type="text"],
+    .login-container input[type="password"],
+    .login-container button {
+        width: 100%;
+        padding: 10px;
+        margin-bottom: 15px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+    }
+    .login-container button {
+        background-color: #007bff;
+        color: #fff;
+        font-size: 16px;
+        cursor: pointer;
+    }
+    .login-container button:hover {
+        background-color: #0056b3;
     }
 </style>
 
+<br> <br>
+<head>
 <div class="login-container">
     <h1>Login</h1>
 
@@ -49,6 +86,7 @@ include 'inc/header.inc.php';
         </div>
     </form>
 </div>
+</head>
 
 
 <?php
